5bdec1ad505da7f67d9fc00c27ee5be7040c1e43
RasmusVraa <98160686+RasmusVraa@users.noreply.github.com>
2023-05-03 14:20:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,26 +12,26 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 client.on('ready', async () => {
   try {
-    const guild = client.guilds.cache.get('1034860943399714888');
+    const guild = client.guilds.cache.get('GUILD_ID'); // ID вашего сервера
     const members = await guild.members.fetch();
 
     for (const [, member] of members) {
       try {
         const dmChannel = await member.createDM();
-        await dmChannel.send('Здравствуйте! Уважаемый Игрок, Вам, как и всем игрокам вручена повестка, чтобы мобилизировать Вас, на второй сезон SplashRP. Повестка, считается полученной, с момента её отправки.');
+        await dmChannel.send('Привет от бота!'); //сообщение рассылки
         await delay(10000); // Задержка в 10 секунд
       } catch (error) {
         if (error.code === 50007) {
-          console.log(`Пропускаю ${member.user.tag}, потому что ЛС закрыт.`);
+          console.log(`Пропускаю ${member.user.tag}, потому что ЛС закрыт.`); //уведомление о там что ЛС пользователя закрыт
         } else {
-          console.error(`Ошибка при отправке сообщения ${member.user.tag}:`, error);
+          console.error(`Ошибка при отправке сообщения ${member.user.tag}:`, error); //сообщение об ошибке отправки сообщения
         }
       }
     }
   } catch (error) {
-    console.error('Ошибка при получении участников сервера:', error);
+    console.error('Ошибка при получении участников сервера:', error); //сообщение ошибки получения игроков
   }
 });
 
 
-client.login('OTg3NDExOTY0NDI4OTc2MTc5.GKJ2eY.tAR-SQZO2IKiPta2asIIr9m4nwEiWRi3tscAfo');
+client.login('BOT_TOKEN'); //токен вашего бота
